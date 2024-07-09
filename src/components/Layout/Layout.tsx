09c3ff85aab8946fbd8c2cbaa8d8845c1747b608
overmind777 <63595243+overmind777@users.noreply.github.com>
2024-07-09 13:38:08 +0300

    add redirect for pages

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -16,9 +16,12 @@ const Layout = () => {
 export default Layout
 
 const Wrapper = styled.div`
-    
+    height: 100%;
     text-align: center;
     padding: 5px;
+
+    background-image: url('/swarm_background.jpg');
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
 `
-
-
